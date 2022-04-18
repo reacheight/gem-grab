3d0cde589445a6d313a7a9f5a3983c62b757f462
Roman Mendaliev <reacheight8@gmail.com>
2022-04-19 01:02:56 +0300

    Add Gem Spawner entity, more correct logic of gem spawning

--- src/game/scripts/vscripts/addon_game_mode.lua
+++ src/game/scripts/vscripts/addon_game_mode.lua
@@ -23,20 +23,28 @@ end
 function CAddonTemplateGameMode:InitGameMode()
 	print( "Template addon is loaded." )
 	GameRules:SetPreGameTime(0)
-	GameRules:GetGameModeEntity():SetThink( "SpawnGem", self, "GemSpawnThink", 2 )
+	GameRules:GetGameModeEntity():SetThink( "SpawnGem", self, "OnGameInProgressThink", 1 )
 end
 
 -- Evaluate the state of the game
 function CAddonTemplateGameMode:SpawnGem()
 	if GameRules:State_Get() == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
-		print("Spawn Gem!")
-		local gem = CreateItem("item_gem", nil, nil)
-		CreateItemOnPositionSync(Vector(16, 16, 0), gem)
-		return 5
-
-	elseif GameRules:State_Get() >= DOTA_GAMERULES_STATE_POST_GAME then
+		local gem_spawner = Entities:FindByName(nil, "gem_spawner")
+		gem_spawner:SetHullRadius(0)
+		
+		GameRules:GetGameModeEntity():SetThink(SpawnGem, self, "GemSpawnThink", 7)
 		return nil
 	end
 
 	return 1
+end
+
+function SpawnGem()
+	print("Spawn Gem!")
+
+	local gem = CreateItem("item_gem", nil, nil)
+	local gem_spawner_position = Entities:FindByName(nil, "gem_spawner"):GetAbsOrigin()
+	local spawn_position = RandomVector(RandomFloat(100, 300)) + gem_spawner_position
+	CreateItemOnPositionSync(spawn_position, gem)
+	return 7
 end
