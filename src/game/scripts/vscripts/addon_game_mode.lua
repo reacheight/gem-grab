138f92ad61d3aeb4b2cb131233057a6db725bea2
Roman Mendaliev <reacheight8@gmail.com>
2022-06-17 20:52:27 +0300

    count team score, win if team score is 10

--- src/game/scripts/vscripts/addon_game_mode.lua
+++ src/game/scripts/vscripts/addon_game_mode.lua
@@ -22,19 +22,76 @@ end
 
 
 function GemGrab:Init()
+  GemGrab.RadiantScore = 0
+  GemGrab.DireScore = 0
+
   GameRules:SetPreGameTime(0)
+  GameRules:SetStrategyTime(0)
+  GameRules:SetShowcaseTime(0)
+  GameRules:SetHeroSelectionTime(10)
+  GameRules:SetTimeOfDay(0.251)
+
+  local GameMode = GameRules:GetGameModeEntity()
+  GameMode:SetDaynightCycleDisabled(true)
+  GameMode:SetAnnouncerDisabled(true)
+  GameMode:SetKillingSpreeAnnouncerDisabled(true)
+  
+  GameRules:SetSameHeroSelectionEnabled(true)
+
   ListenToGameEvent('game_rules_state_change', Dynamic_Wrap(GemGrab, 'OnGameStateChanged'), nil)
+  ListenToGameEvent('dota_item_picked_up', Dynamic_Wrap(GemGrab, 'OnItemPickedUp'), self)
+  ListenToGameEvent('dota_hero_inventory_item_change', Dynamic_Wrap(GemGrab, 'OnItemDropped'), self)
 end
 
 function GemGrab:OnGameStateChanged()
   if GameRules:State_Get() == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
-    GameRules:GetGameModeEntity():SetThink(SpawnGem, self, "GemSpawnThink", 7)
+    GameRules:GetGameModeEntity():SetThink(SpawnGem, self, "GemSpawnThink", 10)
+  end
+end
+
+function GemGrab:OnItemPickedUp(event)
+  local item = EntIndexToHScript(event.ItemEntityIndex)
+  local hero = item:GetParent()
+  local team = hero:GetTeam()
+
+  if event.itemname == "item_gem" then
+    if (team == 2) then
+      GemGrab.RadiantScore = GemGrab.RadiantScore + 1
+
+      if GemGrab.RadiantScore == 10 then
+        GameRules:SetGameWinner(2)
+      end
+    end
+
+    if (team == 3) then
+      GemGrab.DireScore = GemGrab.DireScore + 1
+
+      if GemGrab.DireScore == 10 then
+        GameRules:SetGameWinner(3)
+      end
+    end
+  end
+end
+
+function GemGrab:OnItemDropped(event)
+  if event.removed then
+    local item = EntIndexToHScript(event.item_entindex)
+    if item:GetName() == "item_gem" then
+      local hero = EntIndexToHScript(event.hero_entindex)
+      local team = hero:GetTeam()
+
+      if (team == 2) then
+        GemGrab.RadiantScore = GemGrab.RadiantScore - 1
+      end
+
+      if (team == 3) then
+        GemGrab.DireScore = GemGrab.DireScore - 1
+      end
+    end
   end
 end
 
 function SpawnGem()
-  print("Spawn Gem!")
-
   local gem = CreateItem("item_gem", nil, nil)
   local gem_spawner_position = Entities:FindByName(nil, "gem_spawner"):GetAbsOrigin()
   local spawn_position = RandomVector(RandomFloat(100, 300)) + gem_spawner_position
